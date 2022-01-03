f51d704e51d75bab7557ce194f62836164dce237
davidkocijan <dkocijan@racunarstvo.hr>
2022-01-03 20:45:48 +0100

    Module4Solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -21,7 +21,7 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 
-byeSpeaker.speak = function (name) {
+byeSpeaker.speak = function (name) { 
   console.log(speakWord + " " + name);
 }
 
